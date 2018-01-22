820da1d6e23840fcdfd8395deec2344b5aa2370c
Nadav Bueno <buenonadav@gmail.com>
2018-01-22 08:22:24 +0200

    Fetch FullPost

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import axios from "axios";
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,19 +7,44 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
+    state = {
+        posts: [],
+        selectedPostId: null
+    };
+
+    componentDidMount() {
+        axios
+            .get('https://jsonplaceholder.typicode.com/posts')
+            .then(postsResp => {
+                axios.get('https://jsonplaceholder.typicode.com/users').then(usersResp => {
+                    let posts = postsResp.data.slice(0,4).map(post => {
+                        return {...post, author: usersResp.data[post.userId].name};
+                    });
+
+                    this.setState({posts: posts});
+                });
+            });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
+    render() {
+        let posts = this.state.posts.map(post => {
+            return (<Post key={post.id} title={post.title} author={post.author} click={() => {this.postSelectedHandler(post.id)}} />);
+        });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
-                    <NewPost />
+                    <NewPost/>
                 </section>
             </div>
         );
